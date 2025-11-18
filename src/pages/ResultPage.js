3c69a07a32572764ffd086e4a6925fdfda00bf7c
Thanush V <thanushv466@gmail.com>
2025-11-18 10:44:31 +0530

    sdhd

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -4,75 +4,28 @@ import { useParams } from "react-router-dom";
 
 export default function ResultPage() {
   const { id } = useParams();
-  const [liveResults, setLiveResults] = useState([]);
-  const [savedResult, setSavedResult] = useState(null);
+  const [results, setResults] = useState([]);
 
   const load = async () => {
-    // LIVE RESULTS
     const r = await API.get(`/result/${id}`);
-    setLiveResults(r.data);
-
-    // SAVED RESULT (if exists)
-    try {
-      const s = await API.get(`/result/saved/${id}`);
-      setSavedResult(s.data);
-    } catch (err) {
-      // ignore (saved result not declared yet)
-    }
+    setResults(r.data);
   };
 
-  const declareWinner = async () => {
-    try {
-      const r = await API.post(`/result/declare/${id}`);
-      alert("Winner Declared!");
-      setSavedResult(r.data);
-    } catch (err) {
-      alert(err.response?.data?.msg || "Error");
-    }
-  };
-useEffect(() => {
-    loadResults();
-  }, []);
-
-  if (loading) return <h3>Loading results...</h3>;
+  useEffect(() => load(), []);
 
   return (
     <div style={{ padding: 20 }}>
       <h2>Election Results</h2>
 
-      {/* If saved result exists */}
-      {savedResult ? (
-        <>
-          <h3 style={{ color: "green" }}>
-            🏆 Winner: {savedResult.winner}
-          </h3>
-
-          <h4>Final Declared Results</h4>
-          {savedResult.results.map((r, index) => (
-            <p key={index}>
-              {r.candidateName} — {r.count} votes
-            </p>
-          ))}
-        </>
+      {results.length === 0 ? (
+        <h3>No votes yet</h3>
       ) : (
-        <>
-          <h3>Live Results</h3>
-             {liveResults.length === 0 ? (
-            <p>No votes yet</p>
-          ) : (
-            liveResults.map((r) => (
-              <p key={r.candidateId}>
-                {r.candidateName} — {r.count} votes
-              </p>
-            ))
-          )}
-
-          <button style={{ marginTop: 20 }} onClick={declareWinner}>
-            Declare Winner
-          </button>
-        </>
+        results.map((r, index) => (
+          <p key={index}>
+            {r.candidateName} — {r.count} votes
+          </p>
+        ))
       )}
     </div>
   );
 }
-
